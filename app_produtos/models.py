2e55917c314460bf0adb6ff2b991d323cde39964
Emeson Oliveira Borges <borges2016.leh@gmail.com>
2023-05-12 00:23:12 -0300

    Criando migrations

--- app_produtos/models.py
+++ app_produtos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+
+class nomeproduto(models.Model):
+  nome    = models.CharField(max_length=100)
+  qtdprod = models.IntegerField(max_length=255, verbose_name="Quantidade de Produtos")
+  preco   = models.DecimalField(max_digits=20, decimal_places=2, verbose_name="Preço")
+  descricao = models.CharField(max_length=255, verbose_name="Descrição")
+  
